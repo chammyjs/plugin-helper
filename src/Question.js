ff634b4faf9677ab226f24fe7c96ac8e8d4b74d4
Kornelia Kobiela <korneliak.95@gmail.com>
2017-11-25 12:00:44 +0100

    Document Question class

--- src/Question.js
+++ src/Question.js
@@ -1,14 +1,18 @@
+/**
+ * Question -represents a question for the plugin CLI
+ */
 class Question {
 
 	/**
-	 * constructor - description
+	 * constructor - Question
 	 *
-	 * @param  {type} name                description
-	 * @param  {type} question            description
-	 * @param  {type} answerType          description
-	 * @param  {type} defaultaAnswer = '' description
-	 * @return {type}                     description
+	 * @param  {string} name              name of option
+	 * @param  {string} question          a question that user will receive during creation template
+	 * @param  {type} answerType          the type of answer
+	 * @todo                              writing enum with possible types of answer
+	 * @param  {type} defaultaAnswer = '' default answer if user doesn't write any, if not given, then emtpy string
 	 */
+
 	constructor( name, question, answerType, defaultaAnswer = '' ) {
 		this.name = name;
 		this.question = question;
