9d331e15a729fd19e7b7b948bb1c06d000402545
Kornelia Kobiela <korneliak.95@gmail.com>
2017-11-30 12:00:12 +0100

    Add some documentoation to Type enum

--- src/Type.js
+++ src/Type.js
@@ -1,7 +1,18 @@
 const Enum  = require( 'enumify' );
 
+/**
+ * Represents types of answer
+ */
 class Type extends Enum {}
 
+/**
+ * Available types:
+ * - STRING
+ * - NUMBER
+ * - BOOLEAN
+ * - ARRAY
+ * - OBJECT
+ */
 Type.initEnum( [ 'STRING', 'NUMBER', 	'BOOLEAN', 'ARRAY', 'OBJECT' ] );
 
 module.exports = Type;
